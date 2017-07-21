53b5455f9b82426399e6e1781ec447626a8dfbb7
yfern328 <yfernan2@u.rochester.edu>
2017-07-21 13:58:16 -0400

    Partner lab with Alex P & Yelstin (swapped driver/navigator)

--- spaceship.js
+++ spaceship.js
@@ -8,23 +8,33 @@ class CrewMember {
     if(this.currentShip === "Looking for a Rig"){
       return "had no effect"
     }
+    else if(this.currentShip !== "Looking for a Rig" && this.position === "Pilot") {
+      this.currentShip.warpDrive = "engaged!"
+    }
   }
 
   setsInvisibility(){
     if(this.currentShip === "Looking for a Rig"){
       return "had no effect"
     }
+    else if(this.currentShip !== "Looking for a Rig" && this.position === "Defender") {
+      this.currentShip.cloaked = true
+    }
   }
 
   chargePhasers(){
-      if(this.currentShip === "Looking for a Rig"){
-        return "had no effect"
-      }
+    if(this.currentShip === "Looking for a Rig"){
+      return "had no effect"
     }
+    else if(this.currentShip !== "Looking for a Rig" && this.position === "Gunner") {
+      this.currentShip.phasersCharge = "charged!"
+    }
+  }
 }
 
 class Spaceship {
   constructor(name, crew, phasers, shields, cloaked = false, warpDrive = "disengaged", docked = true, phasersCharge = "uncharged") {
+
       this.name = name
       this.crew = crew
       this.phasers = phasers
@@ -33,6 +43,7 @@ class Spaceship {
       this.warpDrive = warpDrive
 
       if(this.crew.length > 0){
+
         this.docked = false
       }
       else{
@@ -40,5 +51,9 @@ class Spaceship {
       }
 
       this.phasersCharge = phasersCharge
+
+      this.crew.forEach(member => {
+        member.currentShip = this
+      })
   }
 }
